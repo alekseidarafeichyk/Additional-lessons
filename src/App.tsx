aa73a8d797c903f28f296c69a91cab5b38162ff1
alekseidarafeichyk <alekseidarafeichyk@gmail.com>
2020-07-16 11:44:22 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            This is app Component
+            <Rating/>
+            <Accordion/>
+        </div>
+    );
+}
+
+function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
+
+function Star() {
+    return (
+        <div>star</div>
+    )
+}
+
+export default App;
